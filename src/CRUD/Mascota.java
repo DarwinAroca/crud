44cf8358f4ce23f05eb7fa0020a65468f4a49a63
DarwinAroca <darwinaroca@hotmail.com>
2025-07-10 23:23:09 -0500

    UPDATE 10 DE JULIO

---
+++ src/CRUD/Mascota.java
@@ -0,0 +1,73 @@
+package CRUD;
+
+public class Mascota {
+    private String nombre;
+    private String especie;
+    private int edad;
+    private Historial historial;
+    // Constructor
+    public Mascota(String nombre, String especie, int edad) {
+        //*this.nombre = nombre;
+        //this.especie = especie;
+        //this.edad = edad;
+        //Usar setters y historial
+        setNombre(nombre);
+        setEspecie(especie);
+        setEdad(edad);
+        this.historial = new Historial();
+    }
+    public void agregarConsulta(Consulta consulta) {
+        historial.agregarConsulta(consulta);
+    }
+
+    public void mostrarHistorial() {
+        System.out.println("Mascota: " + nombre + " | Especie: " + especie + " | Edad: " + edad + " años");
+        System.out.println("Historial:");
+        historial.mostrarConsultas();
+}
+    // Getter para nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    // Setter para nombre
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getter para especie
+    public String getEspecie() {
+        return especie;
+    }
+
+    // Setter para especie
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    // Getter para edad
+    public int getEdad() {
+        return edad;
+    }
+
+    // Setter para edad con validación
+    public void setEdad(int edad) {
+        if (edad >= 0) {
+            this.edad = edad;
+        } else {
+            System.out.println("Edad no válida.");
+        }
+    }
+
+
+
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad: " + edad);
+    }
+    public void agregarconsulta(Consulta consulta){
+
+    }
+}
+
---
+++ src/CRUD/Propietario.java
@@ -0,0 +1,80 @@
+package CRUD;
+import java.util.ArrayList;
+
+public class Propietario {
+
+    private String nombre;
+    private String documento;
+    private String telefono;
+
+    private ArrayList<Mascota> mascotas = new ArrayList<>();
+
+// Código del Taller 3
+  //  public Propietario(String nombre, long documento, long telefono) {
+    //    this.nombre = nombre;
+      //  this.documento = documento;
+       // this.telefono = telefono;
+   // }
+
+
+    // Uso de setters
+    public Propietario(String nombre, String documento, String telefono) {
+        setNombre(nombre);
+        setDocumento(documento);
+        setTelefono(telefono);
+        this.mascotas = new ArrayList<>();
+}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.isBlank()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacío.");
+        }
+        this.nombre = nombre;
+    }
+
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        if (documento == null || documento.length() < 5) {
+            throw new IllegalArgumentException("Documento inválido, debe tener mínimo 5 caracteres.");
+        }
+        this.documento = documento;
+    }
+
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        if (telefono == null || telefono.length() < 7) {
+            throw new IllegalArgumentException("Teléfono inválido, debe tener mínimo 7 caracteres.");
+        }
+        this.telefono = telefono;
+}
+
+    public void agregarMascota(Mascota m) {
+        if (m != null) {
+            mascotas.add(m);
+ }}
+        public void mostrarMascotas() {
+            System.out.println("Propietario: " + nombre);
+            System.out.println("Documento: " + documento);
+            System.out.println("Numero de Telefono: " + telefono);
+            System.out.println();
+
+            for (Mascota m : mascotas) {
+                m.mostrarHistorial();
+ }
+}
+
+
+ }
+
